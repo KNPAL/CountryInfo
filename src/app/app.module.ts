1ebc5f43145d2299e7e88c7b4a02564df1a756eb
unknown <14sudhansh@gmail.com>
2020-05-07 13:02:41 +0530

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './externalModule/material.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { WorkspaceComponent } from './workspace/workspace.component';
+import { FilterSectionComponent } from './workspace/filter-section/filter-section.component';
+import { GridSectionComponent } from './workspace/grid-section/grid-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    WorkspaceComponent,
+    FilterSectionComponent,
+    GridSectionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/workspace/grid-section/grid-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-section',
+  templateUrl: './grid-section.component.html',
+  styleUrls: ['./grid-section.component.sass']
+})
+export class GridSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
